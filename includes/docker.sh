8d4faecbf9c9929c97f9d1198815c8baff503e64
Alexander Tolstikov <atolstikov@adyax.com>
2020-03-26 11:41:29 +0300

    Refacto docker exec

--- includes/docker.sh
+++ includes/docker.sh
@@ -2,7 +2,29 @@
 
 # shellcheck disable=SC2206
 
-TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE=${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE:-}
+# Possible contexts:
+# - docker
+#   - dind (TOOLBOX_DOCKER_DIND == true)
+# - docker_skip (TOOLBOX_DOCKER_SKIP == true )
+# - shell_to_docker (TOOLBOX_DOCKER_CONTEXT == "SHELL_TO_DOCKER")
+
+export TOOLBOX_DOCKER_CONTEXT=${TOOLBOX_DOCKER_CONTEXT:-SHELL_TO_DOCKER}
+export TOOLBOX_DOCKER_MODE=${TOOLBOX_DOCKER_MODE:-}
+export TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE=${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE:-}
+export TOOLBOX_DOCKER_SKIP=${TOOLBOX_DOCKER_SKIP:-false}
+export TOOLBOX_DOCKER_DIND=${TOOLBOX_DOCKER_DIND:-false}
+
+# Set docker context vars
+if [ -f /.dockerenv ]; then
+  TOOLBOX_DOCKER_CONTEXT="DOCKER"
+  TOOLBOX_DOCKER_MODE="DOCKER"
+  if [ ! "${TOOLBOX_DOCKER_DIND}" == "true" ]; then
+    echo "Inside docker already, setting TOOLBOX_DOCKER_SKIP to true"
+    TOOLBOX_DOCKER_SKIP=true
+  else
+    TOOLBOX_DOCKER_MODE="DIND"
+  fi
+fi
 
 function toolbox_docker_run() {
   _log TRACE "Start 'toolbox_docker_run' function with args: $*"
@@ -14,12 +36,11 @@ function toolbox_docker_run() {
   TOOLBOX_DOCKER_WORKING_DIR=${TOOLBOX_DOCKER_WORKING_DIR:-}
   TOOLBOX_DOCKER_VOLUME_SOURCE=${TOOLBOX_VOLUME_SOURCE:-$(pwd)}
   TOOLBOX_DOCKER_VOLUME_TARGET=${TOOLBOX_DOCKER_VOLUME_TARGET:-${TOOLBOX_DOCKER_VOLUME_SOURCE}}
+  TOOLBOX_DOCKER_MOUNTS=${TOOLBOX_DOCKER_MOUNTS:-}
   # local _TOOLBOX_DOCKER_SSH_FORWARD=${TOOLBOX_DOCKER_SSH_FORWARD:-false}
   TOOLBOX_DOCKER_ENTRYPOINT=${TOOLBOX_DOCKER_ENTRYPOINT:-}
   TOOLBOX_DOCKER_ENV_VARS=${TOOLBOX_DOCKER_ENV_VARS:-}
   TOOLBOX_DOCKER_EXEC_TITLE=${TOOLBOX_DOCKER_EXEC_TITLE-"Execute in docker"}
-
-  TOOLBOX_EXEC_LOG_LEVEL_CMD="DEBUG"
 
   if [[ "$OSTYPE" == "darwin"* ]]; then
     TOOLBOX_DOCKER_MOUNT_OPTIONS=":delegated"
@@ -40,10 +61,28 @@ function toolbox_docker_run() {
     _run_args+=(-t)
   fi
 
+  # Mounts & working dir
   local _run_cmd=("${TOOLBOX_DOCKER_RUN}" \
     $(toolbox_util_array_join "${_run_args[@]}") \
     -w ${TOOLBOX_DOCKER_CURRENT_DIR}/${TOOLBOX_DOCKER_WORKING_DIR} \
     -v ${TOOLBOX_DOCKER_VOLUME_SOURCE}:${TOOLBOX_DOCKER_VOLUME_TARGET}${TOOLBOX_DOCKER_MOUNT_OPTIONS})
+
+  # rm -fR "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp/mounts"
+  if [[ ! -z "${TOOLBOX_DOCKER_MOUNTS}" ]]; then
+    rm -fR "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp/mounts"
+    mkdir -p "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp"
+    cp -fR "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/mounts" "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp/"
+
+    toolbox_exec_hook "toolbox_docker_run_mounts" "before"
+    for i in ${TOOLBOX_DOCKER_MOUNTS//,/ }
+    do
+      # mkdir -p "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp/mounts"
+      if [ -d "${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/mounts$i" ]; then
+        _run_cmd+=(-v ${TOOLBOX_DOCKER_CURRENT_DIR}/toolbox/.tmp/mounts${i}:${i}${TOOLBOX_DOCKER_MOUNT_OPTIONS})
+      fi
+    done
+    toolbox_exec_hook "toolbox_docker_run_mounts" "after"
+  fi
 
   if [[ ! -z "${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE}" ]]; then
     _run_cmd+=(${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE})
@@ -83,8 +122,11 @@ function toolbox_docker_exec() {
   TOOLBOX_DOCKER_SKIP=${TOOLBOX_DOCKER_SKIP:-false}
 
   if [ "${TOOLBOX_DOCKER_SKIP}" == "true" ]; then
-    toolbox_exec "Execute command without docker" ${TOOLBOX_TOOL} "$@"
+    shift
+    toolbox_exec "Execute command without docker" ${TOOLBOX_TOOL_PATH} "$@"
   else
+    toolbox_docker_add_env_var_file_from_prefix "TOOLBOX_"
+
     toolbox_docker_add_env_var_file_from_prefix "$(printf '%s\n' "$TOOLBOX_TOOL_NAME" | awk '{ print toupper($0) }')_"
 
     toolbox_docker_add_env_var_file "${TOOLBOX_TOOL_NAME}.env"
